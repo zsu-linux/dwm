0814f73d897dcd3d231ea36c169c6b2fa45c5044
zsu <zsu.work@gmail.com>
2021-12-07 23:02:22 +0800

    Update config.h

--- config.h
+++ config.h
@@ -8,9 +8,9 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Source Code Pro:size=18",
-                                        "WenQuanYi Micro Hei:size=15:type=Regular:antialias=true:autohint=true",};
-static const char dmenufont[]       = "Source Code Pro:size=18";
+static const char *fonts[]          = { "Source Code Pro:size=18:type=Regular:antialias=true:autohint=true",
+                                        "WenQuanYi Micro Hei:size=15:type=Regular:antialias=true:autohint=true"};
+static const char dmenufont[]       = "Source Code Pro:size=18:type=Regular:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#ffffff";
